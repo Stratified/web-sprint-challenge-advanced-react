402246499940f34e1ec27970bf55bac41aef4f59
Brian Bagrov <Brianbagrov@gmail.com>
2020-08-14 21:22:50 -0400

    MVP reached.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import CheckoutForm from './CheckoutForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test('form header renders', () => {
+	const { getByText } = render(<CheckoutForm />);
+	const header = getByText(/Checkout Form/i);
+	expect(header.textContent).toBe('Checkout Form');
+});
 
-test("form shows success message on submit with form details", () => {});
+test('form shows success message on submit with form details', () => {
+	const { getByTestId } = render(<CheckoutForm />);
+	const checkout = getByTestId(/checkout/i);
+	fireEvent.click(checkout);
+	const successMessage = getByTestId('successMessage');
+	expect(successMessage).toBeInTheDocument();
+});
